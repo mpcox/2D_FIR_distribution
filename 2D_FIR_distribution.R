f088d9131e626098ebe3c4607812f41fb0d46381
mpcox <murray.p.cox@gmail.com>
2022-04-21 18:00:52 +1200

    Adjusted code to allow different colors of subset gene points

--- 2D_FIR_distribution.R
+++ 2D_FIR_distribution.R
@@ -8,15 +8,16 @@ title             <- "RXLRs"
 all_gene_file     <- "example_all_genes.gff3"
 subset_gene_file  <- "example_gene_subset.gff3"
 genome_sizes_file <- "example_genome.dat"
+expression_file   <- "example_expression.dat"  # containing percentile data
 threshold         <- 10000   # maximum size of intergenic distance for plotting
 iterations        <- 1e4     # number of iterations in Monte Carlo simulations
 
 
 # packages
-library(hexbin)
-library(RColorBrewer)
-library(GISTools)
-library(grid)
+require(hexbin)
+require(RColorBrewer)
+require(GISTools)
+require(grid)
 
 
 # functions
@@ -103,6 +104,9 @@ s$prime5 <- s[,4] - s[,10]
 s$prime3 <- s[,11] - s[,5]
 
 # calculate median (subset genes)
+d.med <- median(c(d$prime5, d$prime3), na.rm=T)
+d.med
+
 s.med <- median(c(s$prime5, s$prime3), na.rm=T)
 s.med
 
@@ -114,6 +118,43 @@ d$prime3[d$prime3 >  threshold] <- NA
 d$prime3[d$prime3 < -threshold] <- NA
 
 
+# read expression data
+e <- read.table(expression_file, header=T)
+
+# clean up gene names in dataframe from subset gene GFF3
+s[,9] <- gsub(";+[A-Za-z0-9=]*$", "", s[,9])
+s[,9] <- gsub("ID=", "", s[,9])
+
+# make new percentile column
+s$perc <- rep(0, length(s[,1]))
+
+# add percentile numbers to subset gene dataframe
+for( i in 1:length(s[,1])){
+	
+	s[i,]$perc <- e[which(e$gene == s[i,9]),]$percentile
+}
+
+# set color range
+colors <- rep(0, 100)
+colfunc <- colorRampPalette(c("#FFF5F0", "#67000D")) # Reds
+#colfunc <- colorRampPalette(c("#FCFBFD", "#3F007D")) # Purples
+#colfunc <- colorRampPalette(c("#FFFFCC", "#800026")) # YlOrRd
+colors[0:100] <- colfunc(100)
+
+# make new color column (with zero as white)
+s$color <- rep(0, length(s[,1]))
+for( i in 1:length(s[,1])){
+	
+	if(s[i,]$perc == 0){
+		s[i,]$color <- "#FFFFFF"
+	} else {
+		s[i,]$color <- colors[s[i,]$perc]
+	}
+}
+
+# color legend code
+# plot(1:100, rep(1,100), pch=15, cex=3, col=colors)
+
 # hexbin plot settings
 bin <- hexbin(d$prime3/1000, d$prime5/1000, xbins=40)
 my_colors <- colorRampPalette(rev(add.alpha(brewer.pal(11,'Spectral'),0.5)), alpha=T)
@@ -121,7 +162,7 @@ my_colors <- colorRampPalette(rev(add.alpha(brewer.pal(11,'Spectral'),0.5)), alp
 # hexbin plot
 p <- plot(bin, main=title, colramp=my_colors, legend=F, xlab="3ʹ Flanking Intergenic Distance (Kb)", ylab="5ʹ Flanking Intergenic Distance (Kb)") 
 pushHexport(p$plot.vp)
-grid.points(s$prime3/1000, s$prime5/1000, pch=21, size=unit(6, "pt"), gp=gpar(fill="red"))
+grid.points(s$prime3/1000, s$prime5/1000, pch=21, size=unit(6, "pt"), gp=gpar(fill=s$color))
 upViewport()
 
 
